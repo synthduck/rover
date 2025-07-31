89f78500c7b32be9fc68e9e6c8670c2922b62cbc
synthduck <synth@engineer.com>
2025-07-31 16:52:31 +0000

    Add turn left and backward

--- rover.py
+++ rover.py
@@ -8,8 +8,12 @@ class Rover:
             match command:
                 case "F":
                     self.move_forward()
+                case "B":
+                    self.move_backward()
                 case "R":
                     self.turn_right()
+                case "L":
+                    self.turn_left()
                 
     def move_forward(self):
         x, y = self.position
@@ -21,8 +25,24 @@ class Rover:
             self.position = (x + 1, y)
         elif self.direction == "W":
             self.position = (x - 1, y)
+
+    def move_backward(self):
+        x, y = self.position
+        if self.direction == "N":
+            self.position = (x, y + 1)
+        elif self.direction == "S":
+            self.position = (x, y - 1)
+        elif self.direction == "E":
+            self.position = (x - 1, y)
+        elif self.direction == "W":
+            self.position = (x + 1, y)
     
     def turn_right(self):
         directions = ["N", "E", "S", "W"]
         current_index = directions.index(self.direction)
         self.direction = directions[(current_index + 1) % 4] 
+
+    def turn_left(self):
+        directions = ["N", "W", "S", "E"]
+        current_index = directions.index(self.direction)
+        self.direction = directions[(current_index + 1) % 4]
--- test_rover.py
+++ test_rover.py
@@ -13,7 +13,9 @@ test_command_data = [(0, 0, "S", "F", 0, 1, "S"),
                      (0, 1, "N", "F", 0, 0, "N"),
                      (0, 0, "E", "F", 1, 0, "E"),
                      (1, 0, "W", "F", 0, 0, "W"),
-                     (0, 0, "N", "RR", 0, 0, "S")]
+                     (0, 0, "N", "RR", 0, 0, "S"),
+                     (0, 0, "N", "RFL", 1, 0, "N"),
+                     (0, 0, "N", "RFLB", 1, 1, "N")]
 
 @pytest.mark.parametrize(("start_x", "start_y", "start_direction", "commands", "end_x", "end_y", "end_direction"), test_command_data)
 def test_command_execution(start_x, start_y, start_direction, commands, end_x, end_y, end_direction):
